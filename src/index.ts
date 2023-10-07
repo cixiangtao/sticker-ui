8d4e0f881a14cc22306d4bf1c646ee13a0e9f826
cixiangtao <16064831+cixiangtao@users.noreply.github.com>
2023-10-07 16:28:32 +0800

    feat(registry): wire registry metadata and exports

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+export { Button, buttonVariants } from "../registry/default/button/button"
+export {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  cardVariants,
+} from "../registry/default/card/card"
+export {
+  Label,
+  LabelDescription,
+  LabelMarker,
+  labelTones,
+} from "../registry/default/label/label"
+export {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "../registry/default/table/table"
+export { Tag, tagVariants } from "../registry/default/tag/tag"
